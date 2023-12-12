d8668270e95ddcbf0597ba14bf1909d6df9f1759
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-12-13 03:12:07 +0600

    Better way to subscribe

--- pub-sub-using-redis-stream/agreement.py
+++ pub-sub-using-redis-stream/agreement.py
@@ -1,18 +1,55 @@
 import redis
-
+from typing import List
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def subscribe_to_stream(stream_name, group_name, consumer_name):
+def subscribe_to_stream(stream_handlers: List[any], group_name, consumer_name):
+    for stream_handler in stream_handlers:
+        stream_name = stream_handler["stream_name"]
+        handler = stream_handler["handler"]
+
+        try:
+            redis_client.xgroup_create(stream_name, group_name, mkstream=True)
+        except Exception as e:
+            print(e)
+
     while True:
-        messages_list = redis_client.xreadgroup(group_name, consumer_name, {stream_name: '>'}, count=1, block=0)
-        for messages in messages_list:
-            stream, messages = messages
+        for stream_handler in stream_handlers:
+            stream_name = stream_handler["stream_name"]
+            handler = stream_handler["handler"]
+            
 
-            for message_id, message_data in messages:
-                print(f"Stream: {stream}, Message ID: {message_id}, Data: {message_data}")
+            messages_list = redis_client.xreadgroup(group_name, consumer_name, {stream_name: '>'}, count=1, block=0)
+            for messages in messages_list:
+                stream, messages = messages
 
-                redis_client.xack(consumer_name, stream, message_id)
+                for message_id, message_data in messages:
+                    data = {key.decode('utf-8'): value.decode('utf-8') for key, value in message_data.items()}
+                    # print(data_str)
+                    # print(type(data_str))
+                    handler(data)
+                    # print(f"Stream: {stream}, Message ID: {message_id}, Data: {message_data}")
+
+                    redis_client.xack(consumer_name, stream, message_id)
+
+
+def user_created(data):
+    print("User created", data)
+
+def user_deleted(data):
+
+    print("User deleted", data['id'])
+
+handler_stream = [
+    {
+        "stream_name": "user_created",
+        "handler": user_created
+    },
+    {
+        "stream_name": "user_deleted",
+        "handler": user_deleted
+    }
+]
 
 
 if __name__ == "__main__":
@@ -20,8 +57,14 @@ if __name__ == "__main__":
     group_name = "agreement"  
     consumer_name = "agreement"  
 
+    # try:
+    #     redis_client.xgroup_create(stream_name, group_name, id='0', mkstream=True)
+    # except Exception as e:
+    #     print(e)
+
+
     # redis_client.xgroup_destroy(stream_name, group_name)
 
     # redis_client.xgroup_create(stream_name, group_name, id='0', mkstream=True)
 
-    subscribe_to_stream(stream_name, group_name, consumer_name)
+    subscribe_to_stream(handler_stream, group_name, consumer_name)
--- pub-sub-using-redis-stream/user.py
+++ pub-sub-using-redis-stream/user.py
@@ -9,6 +9,7 @@ def publish_to_stream(stream_name, message):
 
 # Example usage
 if __name__ == "__main__":
-    data =  {"id":str(uuid4()), "first_name": "John", "last_name": "Doe" }
-    publish_to_stream('user_created',data,)
+    data =  {"id":"1ad0109e-fea4-4afc-8d3b-ce726fd054c6", "firstname": "Taki", "lastname": "Uddin", "email": "taki+104@user.com" }
+    publish_to_stream('user_created',data)
+    # publish_to_stream('user_deleted',data,)
     print("data sent to stream",data)
